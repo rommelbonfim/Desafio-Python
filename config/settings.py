fb2788da17b5d0ad3aa25250c18499669965dc24
rommelbonfim <70414415+rommelbonfim@users.noreply.github.com>
2024-05-18 18:14:18 -0300

    criação models

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -90,6 +91,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
--- mercado/models.py
+++ mercado/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    email = models.EmailField(unique=True)
+    pass
+
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=30)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return self.nome
+
+
+class Pedido(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+
+
+    def __str__(self):
+        return self.usuario
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='itens_pedido')
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField(default=1)
